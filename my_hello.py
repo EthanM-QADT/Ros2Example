fcb4180789c9c9c838b48a75535230e4995bc874
EthanM-QADT <22knc5@queensu.ca>
2025-01-27 16:12:35 -0500

    Create my_hello.py

---
+++ my_hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+# Create MyNode node inherriting from Node
+class MyNode(Node):
+  def __init__(self):
+    # Initalize node and define node name
+    super().__init__("first_node")
+    # Add additional functionality, such as logging hello
+    self.get_logger().info("HELLO QADT")
+
+def main *args-None):
+  # Initilize ros2 coms
+  rcply.init(args=args)
+  # Create the node
+  node = MyNode()
+  # Shutdown ros2 coms
+  rcply.shutdown()
+
+# Standard python convention for executable files
+if __name__ == '__main__':
+  main()
+
